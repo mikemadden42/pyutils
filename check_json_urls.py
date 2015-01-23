56c9f7bc9b556732486dea16edd9bf3c442d1c43
Michael Madden <mikemadden42@icloud.com>
2015-01-22 20:41:55 -0600

    Added check_json_urls.py

---
+++ check_json_urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.4
+
+import requests
+
+
+def check_urls():
+    with open('urls.txt', 'r') as f:
+        for url in f:
+            try:
+                r = requests.get(url)
+                print((r.json()))
+            except ValueError as msg:
+                print(('FAIL:', url, r.status_code))
+
+if __name__ == '__main__':
+        check_urls()
