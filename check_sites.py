856a893180daf68964bd9ae5c1b0a9f3281077f0
Michael Madden <mikemadden42@icloud.com>
2015-03-10 15:39:28 -0500

    Initial version of check_sites.py. Uses standard library only

---
+++ check_sites.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# http://pymotw.com/2/multiprocessing/basics.html
+
+import multiprocessing
+import os
+import urllib2
+
+
+def get(url):
+    try:
+        f = urllib2.urlopen(url)
+        # if f.msg == 'OK':
+        #     print 'url:', f.url
+        #     print 'code:', f.code
+        #     print 'message:', f.msg
+        #     data = f.read()
+        #     print 'bytes read:', len(data)
+        #     print
+    except Exception, e:
+        print url
+        print e
+        print
+
+
+def check():
+    with open('urls.txt', 'r') as file:
+        jobs = []
+        for url in file:
+            url = url.rstrip(os.linesep)
+            p = multiprocessing.Process(target=get, args=(url,))
+            jobs.append(p)
+            p.start()
+
+
+if __name__ == '__main__':
+    check()
