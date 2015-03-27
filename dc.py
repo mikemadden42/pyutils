7e06263213fe6404ddb6a91209ddce8062da4745
Michael Madden <mikemadden42@gmail.com>
2015-03-27 14:34:17 -0500

    Initial version of dc.py

---
+++ dc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+
+
+def dc():
+    host = 'www.alamo.ca'
+
+    try:
+        ip = socket.gethostbyname(host)
+    except socket.error, msg:
+        print host, msg
+        sys.exit(1)
+
+    if ip.startswith('65'):
+        print 'ETC'
+    elif ip.startswith('12'):
+        print 'PTC'
+    else:
+        print 'UNKNOWN'
+
+
+if __name__ == '__main__':
+    dc()
