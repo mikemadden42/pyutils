12707841ad8376116f4bb6a41a4d7a35923a187f
Michael Madden <mikemadden42@gmail.com>
2015-03-28 21:18:51 -0500

    Added thread pool to lookup.py

--- lookup.py
+++ lookup.py
@@ -2,17 +2,31 @@
 
 import os
 import socket
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import cpu_count
+
+
+def find(host):
+    try:
+        ip = socket.gethostbyname(host)
+        return(host, ip)
+    except socket.error as msg:
+        print(host, msg)
 
 
 def lookup():
-    with open('hosts.txt', 'r') as f:
-        for host in f:
-            host = host.rstrip(os.linesep)
-            try:
-                ip = socket.gethostbyname(host)
-                print(host, ip)
-            except socket.error as msg:
-                print(host, msg)
+    f = open('hosts.txt', 'r')
+    urls = [x.strip() for x in f.readlines()]
+
+    pool = ThreadPool(cpu_count() * 4)
+    results = pool.map(find, urls)
+
+    for r in results:
+        if r is not None:
+            print(r)
+
+    pool.close()
+    pool.join()
 
 if __name__ == '__main__':
     lookup()
