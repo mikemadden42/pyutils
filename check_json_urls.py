b8fc6fd2c9cc94f3a5c9f41895ffbd95b028eea9
Michael Madden <mikemadden42@gmail.com>
2015-07-08 16:15:10 -0500

    Clean up check_json_urls.py from pylint.

--- check_json_urls.py
+++ check_json_urls.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
+"""Check URLs with a JSON endpoint"""
+
 import os
 import requests
 
 
 def check_urls():
-    with open('json_urls.txt', 'r') as f:
-        for url in f:
+    """Check URLs with a JSON endpoint"""
+    with open('json_urls.txt', 'r') as infile:
+        for url in infile:
             url = url.rstrip(os.linesep)
             try:
-                r = requests.get(url)
-                print(r.json())
+                data = requests.get(url)
+                print(data.json())
             except ValueError as msg:
-                print(('FAIL:', url, r.status_code, msg))
+                print(('FAIL:', url, data.status_code, msg))
 
 if __name__ == '__main__':
-        check_urls()
+    check_urls()
