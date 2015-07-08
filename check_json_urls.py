ef3a330ebc52fef17fd4bdb88bbb17a67ab35868
Michael Madden <mikemadden42@gmail.com>
2015-07-08 15:04:21 -0500

    Deleted check_sites.py & youtube.py. Added json_urls.txt. Fixed check_json_urls.py.

--- check_json_urls.py
+++ check_json_urls.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
+import os
 import requests
 
 
 def check_urls():
-    with open('urls.txt', 'r') as f:
+    with open('json_urls.txt', 'r') as f:
         for url in f:
+            url = url.rstrip(os.linesep)
             try:
                 r = requests.get(url)
-                print((r.json()))
+                print(r.json())
             except ValueError as msg:
-                print(('FAIL:', url, r.status_code))
+                print(('FAIL:', url, r.status_code, msg))
 
 if __name__ == '__main__':
         check_urls()
