6753cedd2b8b7d108762c695243a9eca8d6d4322
Michael Madden <mikemadden42@gmail.com>
2015-07-09 13:52:33 -0500

    Clean up cpus.py from pylint.

--- cpus.py
+++ cpus.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+"""Get the number of cpus"""
+
 # http://stackoverflow.com/questions/1006289/how-to-find-out-the-number-of-cpus-using-python
 
 import multiprocessing
 
 
 def cpus():
+    """Get the number of cpus"""
     print('CPUs: %d' % (multiprocessing.cpu_count()))
 
 if __name__ == '__main__':
