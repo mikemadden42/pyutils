311213832719a7819bcc6f067af4f65af0aad4b2
Michael Madden <mikemadden42@gmail.com>
2015-08-15 09:20:31 -0500

    Initial version of check_links.py

---
+++ check_links.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Find broken links in a directory"""
+
+import os
+
+
+def check_links(directory):
+    """Find broken links in a directory"""
+    files = os.listdir(directory)
+    for filename in files:
+        full_path = os.path.join(directory, filename)
+        if not os.path.exists(full_path):
+            print full_path
+
+if __name__ == '__main__':
+    check_links('/etc/alternatives')
+    check_links('/usr/bin')
