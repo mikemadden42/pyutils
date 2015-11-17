5d645a5f6f7d5d38a0acfc724f7cee452b49a8bb
Michael Madden <mikemadden42@icloud.com>
2015-11-17 13:36:39 -0600

    Added boot time.  Modified cpu info

--- sysinfo.py
+++ sysinfo.py
@@ -2,21 +2,31 @@
 
 """Get system information"""
 
+import datetime
 import multiprocessing
+import psutil
 import six
-from psutil import virtual_memory
+
+# http://stackoverflow.com/questions/12400256/python-converting-epoch-time-into-the-datetime
+def boot():
+    """Get the boot time"""
+    boot = psutil.boot_time()
+    six.print_('Boot time: %s' % (datetime.datetime.fromtimestamp(boot).strftime('%Y-%m-%d %H:%M:%S')))
 
 # http://stackoverflow.com/questions/1006289/how-to-find-out-the-number-of-cpus-using-python
 def cpus():
     """Get the number of cpus"""
-    six.print_('CPUs: %d' % (multiprocessing.cpu_count()))
+    six.print_('Total CPUs: %d' % (multiprocessing.cpu_count()))
+    six.print_('Physical CPUs: %d' % (psutil.cpu_count(logical=False)))
+    six.print_('Logical CPUs: %d' % (psutil.cpu_count()))
 
 # http://stackoverflow.com/questions/22102999/get-total-physical-memory-from-python
 def memory():
     """Get the amount of sytem memory"""
-    mem = virtual_memory()
+    mem = psutil.virtual_memory()
     six.print_('Memory: %d' % (mem.total))
 
 if __name__ == '__main__':
+    boot()
     cpus()
     memory()
