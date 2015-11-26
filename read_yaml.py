bf61c6b3716f07161f4336c060655af0ca25ef62
Michael Madden <mikemadden42@icloud.com>
2015-11-26 14:07:21 -0600

    Initial version of config.yml & read_yaml.py

---
+++ read_yaml.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+Demo script for reading YAML.
+"""
+
+import yaml
+
+
+# http://martin-thoma.com/configuration-files-in-python/
+def read_yaml():
+    """Read & print YAML."""
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+        for section in cfg:
+            print section
+            print cfg['mysql']
+            print cfg['other']
+
+if __name__ == '__main__':
+    read_yaml()
