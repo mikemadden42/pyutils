58aae9854b2f097d6bb144b8452ee8264c4f4bc6
Michael Madden <mikemadden42@icloud.com>
2015-12-28 21:43:03 +0000

    Initial version of nfl_scores.py

---
+++ nfl_scores.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Get the current NFL scores in json"""
+
+import requests
+import six
+
+# https://code.google.com/p/nfl-game-stats/
+# http://www.nfl.com/liveupdate/scorestrip/ss.json
+# http://www.nfl.com/liveupdate/scorestrip/postseason/ss.xml
+
+
+def nfs_scores():
+    """Get the current NFL scores"""
+
+    url = 'http://www.nfl.com/liveupdate/scorestrip/ss.json'
+    response = requests.get(url)
+    data = response.json()
+
+    week = data['w']
+    year = data['y']
+    six.print_('%d WEEK %d' % (year, week))
+    for game in data['gms']:
+        try:
+            six.print_(game['h'], game['hs'], game['v'], game['vs'])
+        except UnicodeEncodeError:
+            pass
+
+
+if __name__ == '__main__':
+    nfs_scores()
