50c78575ad32ac180a22e19ffa8b5182d5ba1726
Michael Madden <mikemadden42@icloud.com>
2016-02-26 10:52:07 -0600

    Initial version of mail_test.py

---
+++ mail_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""Simple SMTP test that sends an email"""
+
+# https://docs.python.org/2/library/smtplib.html
+
+import os
+import sys
+import smtplib
+
+
+def prompt(message):
+    """Simple prompt method"""
+    return raw_input(message).strip()
+
+
+def mail_test():
+    """Simple SMTP test"""
+    print 'Hello %s on %s.' % (os.getlogin(), sys.platform)
+
+    fromaddr = prompt("From: ")
+    toaddrs = prompt("To: ").split()
+    print "Enter message, end with ^D (Unix) or ^Z (Windows):"
+
+    # Add the From: and To: headers at the start!
+    msg = ("From: %s\r\nTo: %s\r\n\r\n" % (fromaddr, ", ".join(toaddrs)))
+    while 1:
+        try:
+            line = raw_input()
+        except EOFError:
+            break
+        if not line:
+            break
+        msg = msg + line
+
+    print "Message length is " + repr(len(msg))
+
+    server = smtplib.SMTP('localhost')
+    server.set_debuglevel(1)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
+
+if __name__ == '__main__':
+    mail_test()
