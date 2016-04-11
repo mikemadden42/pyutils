b9aaeae8471d504b48279bf4feafe5f3684a724c
Michael Madden <mikemadden42@icloud.com>
2016-04-11 11:59:45 -0500

    Added more support for Python 3

--- mail_test.py
+++ mail_test.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
-
 """Simple SMTP test that sends an email"""
 
 # https://docs.python.org/2/library/smtplib.html
 
 import smtplib
+import six
+
+from six.moves import input
 
 
 def prompt(message):
     """Simple prompt method"""
-    return raw_input(message).strip()
+    return input(message).strip()
 
 
 def mail_test():
     """Simple SMTP test"""
     fromaddr = prompt("From: ")
     toaddrs = prompt("To: ").split()
-    print "Enter message, end with ^D (Unix) or ^Z (Windows):"
+    six.print_("Enter message, end with ^D (Unix) or ^Z (Windows):")
 
     # Add the From: and To: headers at the start!
     msg = ("From: %s\r\nTo: %s\r\n\r\n" % (fromaddr, ", ".join(toaddrs)))
     while 1:
         try:
-            line = raw_input()
+            line = input()
         except EOFError:
             break
         if not line:
             break
         msg = msg + line
 
-    print "Message length is " + repr(len(msg))
+    six.print_("Message length is " + repr(len(msg)))
 
     server = smtplib.SMTP('localhost')
     server.set_debuglevel(1)
--- quotes.py
+++ quotes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-
 """Get the current stock quotes"""
 
 import requests
+import six
 
 
 # http://stackoverflow.com/questions/27794418/free-json-formatted-stock-quote-api-live-or-historical
@@ -10,8 +10,8 @@ def quotes():
     """Get the current stock quotes"""
     url = 'http://www.google.com/finance/info?q=NSE:GOOGL,AAPL,MSFT,FB'
     response = requests.get(url)
-    print response.status_code
-    print response.ok
+    six.print_(response.status_code)
+    six.print_(response.ok)
 
 
 if __name__ == '__main__':
