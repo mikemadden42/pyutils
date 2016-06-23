cf48990585300b2f8f1074a9d58773da292c8d64
Michael Madden <mikemadden42@icloud.com>
2016-06-23 14:11:28 -0500

    Made updates for Python 3.

--- empty_dirs.py
+++ empty_dirs.py
@@ -4,15 +4,16 @@
 # http://stackoverflow.com/questions/26774892/how-to-find-recursively-empty-directories-in-python
 
 import os
+import six
 
 
 def empty_dirs(root_dir='.'):
     """Find empty directories."""
-    print root_dir
+    six.print_(root_dir)
     for dirpath, dirs, files in os.walk(root_dir):
         if not dirs and not files:
             yield dirpath
 
 
 if __name__ == '__main__':
-    print list(empty_dirs('/tmp'))
+    six.print_(list(empty_dirs('/tmp')))
--- pkg_check.py
+++ pkg_check.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import gzip
 import os
 import urllib
+import six
 
 
 def check():
@@ -15,21 +16,21 @@ def check():
     url = 'http://reflection.oss.ou.edu/raspbian/raspbian/dists/jessie/main/binary-armhf/Packages.gz'
 
     if not os.path.exists(all_pkgs):
-        print 'Downloading package list...'
+        six.print_('Downloading package list...')
         urllib.urlretrieve(url, all_pkgs)
 
-    with gzip.open(all_pkgs, 'r') as pkgs:
+    with gzip.open(all_pkgs, 'rt') as pkgs:
         for line in pkgs:
             line = line.rstrip(os.linesep)
             if line.startswith('Package'):
                 pkg = line.split(': ')[1]
                 packages[pkg] += 1
 
-    with open(required_pkgs, 'r') as required:
+    with open(required_pkgs, 'rt') as required:
         for line in required:
             line = line.rstrip(os.linesep)
             if line not in packages:
-                print line
+                six.print_(line)
 
 
 if __name__ == '__main__':
