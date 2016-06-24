c8933144eeb19abe7c816d4f17573c27f6bc1fa8
Michael Madden <mikemadden42@icloud.com>
2016-06-24 12:45:17 -0500

    Change print to six.print_.

--- check_gitlab_health.py
+++ check_gitlab_health.py
@@ -3,12 +3,13 @@
 
 import os
 from collections import defaultdict
+import six
 
 
 def check_shell(logfile):
     """Scan the gitlab nginx logs for any health issues."""
     warnings = defaultdict(int)
-    print 'ERRORS IN %s:' % (logfile)
+    six.print_('ERRORS IN %s:' % (logfile))
     with open(logfile) as infile:
         for line in infile:
             line = line.rstrip(os.linesep)
@@ -17,15 +18,15 @@ def check_shell(logfile):
                 warnings[warning] += 1
 
     for key, val in warnings.iteritems():
-        print '"%s" repeated %d times' % (key, val)
-    print
+        six.print_('"%s" repeated %d times' % (key, val))
+    six.print_()
 
 
 def check_nginx(logfile):
     """Scan the gitlab nginx logs for any health issues."""
-    print 'ERRORS IN %s:' % (logfile)
+    six.print_('ERRORS IN %s:' % (logfile))
     with open(logfile) as infile:
-        print infile.read()
+        six.print_(infile.read())
 
 
 if __name__ == '__main__':
