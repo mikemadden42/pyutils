5ffe9e31f0b4a121da6d59e938493e61ffa94705
Michael Madden <mikemadden42@icloud.com>
2016-07-05 14:45:39 -0500

    Added check_load.py.
    Fixed pylint errors.

---
+++ check_load.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Check load and email alert if load is high."""
+
+import platform
+import smtplib
+
+
+def mail_alert(load):
+    """Email alert if load is high."""
+    subject = 'load on %s - %.2f' % (platform.node(), load)
+    sender = 'from@fastmail.com'
+    receivers = 'to@fastmail.com'
+
+    message = ("Subject: %s\r\nFrom: %s\r\nTo: %s\r\n\r\n" %
+               (subject, sender, receivers))
+    try:
+        server = smtplib.SMTP('localhost')
+        server.sendmail(sender, receivers, message)
+        print "Successfully sent email"
+    except smtplib.SMTPException:
+        print "Error: unable to send email"
+
+
+def check_load():
+    """Check load."""
+    with open('/proc/loadavg') as infile:
+        for line in infile:
+            stats = line.split()
+            load_avg = float(stats[2])
+            if load_avg > 1.00:
+                mail_alert(load_avg)
+
+
+if __name__ == '__main__':
+    check_load()
