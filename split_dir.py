1f99e9bf655e4cb516bb6ff8f3fc2ef8ac4d934f
Michael Madden <mikemadden42@icloud.com>
2019-08-31 21:15:56 -0500

    Pass in directory and number via command line.

--- split_dir.py
+++ split_dir.py
@@ -5,23 +5,24 @@
 # https://docs.python.org/2/library/shutil.html
 """Split a large directory into smaller directories"""
 
+import argparse
 import os
 import shutil
 
 
-def split_dir(directory):
+def split_dir(directory, n):
     """Split a large directory into smaller directories"""
 
     files = os.listdir(directory)
     size = len(files)
 
-    batches = int(size / 100)
+    batches = int(size / n)
     i = 0
 
     while i <= batches:
         os.mkdir(directory + "/" + str(i))
-        start = i * 100
-        end = i * 100 + 100
+        start = i * n
+        end = i * n + n
         part = files[start:end]
 
         for part_file in part:
@@ -31,4 +32,8 @@ def split_dir(directory):
 
 
 if __name__ == "__main__":
-    split_dir("dir1")
+    PARSER = argparse.ArgumentParser(description="split up a large directory")
+    PARSER.add_argument("-d", "--directory", help="directory", required=True)
+    PARSER.add_argument("-n", "--number", help="number", required=True)
+    ARGS = vars(PARSER.parse_args())
+    split_dir(ARGS["directory"], int(ARGS["number"]))
