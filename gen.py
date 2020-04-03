e6f0a93f4d71279e9bb958f6eb5dc8b15aa5bce7
Michael Madden <mikemadden42@icloud.com>
2020-04-03 12:57:15 -0500

    Add generator example

---
+++ gen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Hello world function"""
+
+import random
+
+
+def rand_int():
+    """Generate int between 0 and 100."""
+    while True:
+        yield random.randrange(100)
+
+
+def main():
+    """Demo for random number generator."""
+    # Print out 8 random numbers.
+    gen = rand_int()
+    for i in range(8):
+        print(next(gen))
+
+    # Print out random numbers forever.
+    for i in rand_int():
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
