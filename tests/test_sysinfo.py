0c632577907c1c88b757b94ffb5396a674feaa28
Michael Madden <mikemadden42@icloud.com>
2020-05-11 15:04:28 -0500

    Require at least 512 MB RAM

--- tests/test_sysinfo.py
+++ tests/test_sysinfo.py
@@ -4,12 +4,12 @@ import sysinfo
 
 
 def test_cores():
-    # Ensure there is at least 2 cores.
+    # Ensure there is at least 1 core.
     result = sysinfo.get_cores()
     assert result >= 1
 
 
 def test_memory():
-    # Ensure there is at least 4 GB.
+    # Ensure there is at least 512 MB.
     result = sysinfo.get_memory()
-    assert result >= 1073741824
+    assert result >= 536870912
