c89f51c797154b71a6fb79c89c73ce924f7ea1be
Michael Madden <mikemadden42@icloud.com>
2020-06-14 12:36:55 -0500

    Exit when changelog does not exist

--- read_changelog.py
+++ read_changelog.py
@@ -6,17 +6,20 @@ import yaml
 
 def read_changelog():
     """Read interum changelog file."""
-    with open(r"changelog.yml") as file:
-        changelog = yaml.safe_load(file)
+    try:
+        with open(r"changelog.yml") as file:
+            changelog = yaml.safe_load(file)
 
-        for change in changelog:
-            print(change)
-            print("- author:", changelog[change]["meta"]["author"])
-            print("- description:", changelog[change]["meta"]["description"])
-            print("- hash:", changelog[change]["meta"]["hash"])
-            print("- url:", changelog[change]["meta"]["url"])
-            print("- tags:", changelog[change]["meta"]["tags"])
-            print("- changelog:", changelog[change]["changelog"])
+            for change in changelog:
+                print(change)
+                print("- author:", changelog[change]["meta"]["author"])
+                print("- description:", changelog[change]["meta"]["description"])
+                print("- hash:", changelog[change]["meta"]["hash"])
+                print("- url:", changelog[change]["meta"]["url"])
+                print("- tags:", changelog[change]["meta"]["tags"])
+                print("- changelog:", changelog[change]["changelog"])
+    except FileNotFoundError as error:
+        print(error)
 
 
 if __name__ == "__main__":
