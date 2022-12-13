b4ad493928c252a8fb43987ff579ff54597ae20a
Michael Madden <mikemadden42@icloud.com>
2022-12-12 21:31:20 -0600

    Filter connection, status correctly.

--- list_lan_hosts.py
+++ list_lan_hosts.py
@@ -7,11 +7,17 @@ def list_hosts():
     df = pd.read_csv("lan-hosts.tsv", sep="\t")
     connections = ["Ethernet", "Wi-Fi"]
     for connection in connections:
-        print(df[df["Connection"] == connection], end="\n\n")
+        print(connection)
+        # https://stackoverflow.com/questions/11350770/filter-pandas-dataframe-by-substring-criteria
+        conn_df = df[df["Connection"].str.contains(connection)]
+        if not conn_df.empty:
+            print(conn_df, end="\n\n")
 
     statuses = ["on", "off"]
     for status in statuses:
-        print(df[df["Status"] == status], end="\n\n")
+        status_df = df[df["Status"].str.contains(status)]
+        if not status_df.empty:
+            print(status_df, end="\n\n")
 
 
 if __name__ == "__main__":
